16f8891d9cb33c512d18d6cac7e3112883f4e4eb
Sonu Abraham <sonu.abraham@rfi.com.au>
2023-06-01 21:08:19 +1000

    Added app stage

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -1,17 +1,31 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
+import { MyPipelineAppStage } from "./my-pipeline-app-stage";
 
 export class MyPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'MyPipeline',
-      synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('sonuabraham/ci-cd-pipeline', 'master'),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth']
-      })
+    const pipeline = new CodePipeline(this, "Pipeline", {
+      pipelineName: "MyPipeline",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub(
+          "sonuabraham/ci-cd-pipeline",
+          "master"
+        ),
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
     });
+
+    pipeline.addStage(
+      new MyPipelineAppStage(this, "test", {
+        env: { account: "104612892635", region: "us-east-1" },
+      })
+    );
   }
 }
