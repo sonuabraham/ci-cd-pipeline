a1ff0a08e36369a11fd90a8d537aee4710ded55e
Sonu Abraham <sonu.abraham@rfi.com.au>
2023-06-01 20:33:34 +1000

    Initial commit

---
+++ bin/my-pipeline.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { MyPipelineStack } from '../lib/my-pipeline-stack';
+
+const app = new cdk.App();
+new MyPipelineStack(app, 'MyPipelineStack', {
+  env: {
+    account: "104612892635",
+    region: "us-east-1",
+  }
+});
+
+app.synth();
